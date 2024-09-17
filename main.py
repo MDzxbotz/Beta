6bea95e4f6c833bedc2e4cfab4e9d41420c3dbf0
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2024-09-17 23:34:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ with open("File.txt", "a+") as file:
      file.write("hi hello")
      response = requests.post(url, files={'file': file})
      response.raise_for_status()
+     print(response)
      print(response.json())
