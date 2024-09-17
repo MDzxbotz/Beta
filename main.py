ec082ce51da50c78510e81f7fb6d1d838bdc3c3f
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2024-09-17 23:32:23 +0530

    Update main.py9

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ url = "https://envs.sh"
 with open("File.txt", "a+") as file:
      file.write("hi hello")
      response = requests.post(url, files={'file': file})
+     response.raise_for_status()
      print(response.json())
